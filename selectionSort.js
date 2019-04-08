73b63b23813191093bd6cadbc6d7e0c1ed79edc2
Luke Fiorio <lukefiorio@Lukes-MacBook-Pro.local>
2019-04-07 15:50:03 -1000

    initial commit

--- selectionSort.js
+++ selectionSort.js
@@ -1,15 +1,1 @@
-function selectionSort(arr) {
-  let minIndex;
-  for (let j = 0; j < arr.length - 1; j++) {
-    let minVal = arr[j];
-    for (let i = j; i < arr.length; i++) {
-      if (arr[i] <= minVal) {
-        minVal = arr[i];
-        minIndex = i;
-      }
-    }
-    const temp = arr[j];
-    arr[j] = arr[minIndex];
-    arr[minIndex] = temp;
-  }
-}
+function selectionSort(arr) {}
